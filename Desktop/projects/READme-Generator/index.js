74c156388bf916ce40351decac30c55b9ed68322
Aisha Kimpson <aishakimpson@Aishas-MacBook-Air.local>
2022-04-03 23:24:47 -0400

    commented out const util

--- Desktop/projects/READme-Generator/index.js
+++ Desktop/projects/READme-Generator/index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require('fs');
-const util = require("util");
+//const util = require("util");
 
 const generateMarkdown = require('./utils/generateMarkdown');
 
